448db3f55c77498c8f9c1f2c2ad257bccd44c146
Danish Hisham <mddanishhisham@gmail.com>
2022-08-24 16:56:07 +0800

    Updated new fields for de
    exoit
    q

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :bookings
-  resources :watches
-  resources :users
+  root to: 'watches#index'
+
+  resources :watches do
+    resources :bookings, only: %i[new create]
+  end
+  # resources :bookings, only: %i[edit update]
+
+  # resources :users
 end
