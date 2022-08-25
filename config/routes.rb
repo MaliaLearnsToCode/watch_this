1d97faa5ccd07f6b39e128be29061bf6dc8dfc64
Malia Nguyen <dr.malia.nguyen@gmail.com>
2022-08-25 17:27:14 +0800

    route change

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
 
-  root "pages#home"
-  resources :watches
-  resources :users
+
   # root to: 'watches#index'
 
   root "watches#index"
